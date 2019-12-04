f234c4092ca2a64072f7600127f49982023da1d8
samy <39716963+samykhelifa@users.noreply.github.com>
2019-12-04 03:45:18 +0100

    Update Test.java

--- chapter1/Test.java
+++ chapter1/Test.java
@@ -1,35 +1,76 @@
 
- interface I2
-{
-    final String s = "I2";
-    static void method1() 
-    {
-        System.out.println(s);
-    }
+ import java.util.*;
 
-    default String method2(String x)
+@FunctionalInterface
+interface PetMatcher
+{
+    List<Pet> match(Pet pet);
+    default Pet first(Pet pet)
     {
-        return s + x; 
+        int index = Pet.pets.indexOf(pet);
+        return index > -1? Pet.pets.get(index) : null;
     }
 }
 
-class C2 implements I2
-{
-    @Override 
-    public String method2(String x) { return x; }
-}
 
-class C3 implements I2 {}
 
- public class Test
+
+public class PlayfulPets 
 {
+    private static void matchPet(String criteria, 
+                                 PetMatcher matcher, Pet pet)
+    {
+        System.out.println("\n" + criteria + ":");
+        System.out.println("First: " + matcher.first(pet));
+        System.out.println("All matches:");
+        List<Pet> matches = matcher.match(pet);
+        for (Pet p : matches)
+            System.out.println(p);
+    }
+
     public static void main(String[] args)
     {
-        I2.method1();
-        I2 objC2 = new C2();
-        I2 objC3 = new C3();
-
-        System.out.println(objC2.method2("Hello"));
-        System.out.println(objC3.method2("World"));
+        Pet.pets.add(new Pet("Scruffy","dog","poodle",
+                             "white",895.00));
+        Pet.pets.add(new Pet("Meow","cat","siamese","white",740.25));
+        Pet.pets.add(new Pet("Max","dog","poodle","black",540.50));
+        Pet.pets.add(new Pet("Cuddles","dog","pug","black",1282.75));
+        Pet.pets.add(new Pet("Slider","snake","garden",
+                             "green",320.00));
+        
+        PetMatcher breedMatcher = new PetMatcher() {
+            public List<Pet> match(Pet pet)
+            {
+                List<Pet> matches = new ArrayList<>();
+                for (Pet p : Pet.pets)
+                    if (p.equals(pet))
+                        matches.add(p);
+                return matches;
+            }
+        };
+        
+        PetMatcher priceMatcher = new PetMatcher() {
+            public List<Pet> match(Pet pet)
+            {
+                List<Pet> matches = new ArrayList<>();
+                for (Pet p : Pet.pets)
+                    if (p.price <= pet.price)
+                        matches.add(p);
+                return matches;
+            }
+            public Pet first(Pet pet)
+            {
+                int index = -1;
+                for(Pet p : Pet.pets)
+                    if (p.price <= pet.price)
+                        return p;
+                return null;
+            }
+        };
+        
+        matchPet("Poodles",breedMatcher,
+                 new Pet(null, "dog", "poodle", null, 0.0));
+        matchPet("Pets for $800 or less",priceMatcher,
+                 new Pet(null, null, null, null, 800.0));
     }
 }
